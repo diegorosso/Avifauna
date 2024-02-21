f4fa2fd529f1557f196ce2e3ac97c9413deddcf9
macarena <macarena@blockinar.com>
2024-02-21 19:03:43 -0300

    Add router

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import FamilyView from '../views/FamilyView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/familia/:familyName',
+      name: 'family',
+      component: FamilyView,
+      props: true,
+    }
+  ]
+})
+
+export default router
